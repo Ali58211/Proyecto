d612e1ccae197bd276bc9a3e7150c1b7a1a15aca
ManuelPociello <166668032+ManuelPociello@users.noreply.github.com>
2026-06-14 00:11:51 -0300

    f

--- Program.cs
+++ Program.cs
@@ -12,67 +12,98 @@
                 Usuario usuario_activo = new Usuario();
                 while (continuar)
                 {
-                    cadena = Utildades.menu( new String[] { "Iniciar Secion", "Crear Usuario", "Salir" });
-                    switch (cadena)
+                    if (!sesion_iniciada)
                     {
-                        case "Iniciar Secion":
+                        cadena = Utildades.menu( new String[] { "Iniciar Secion", "Crear Usuario", "Salir" });
+                        switch (cadena)
                         {
-                            sesion_iniciada = Utildades.Iniciar_Sesion(ref usuario_activo);
-                            break;
-                        }
-                        case "Crear Usuario":
-                        {
-                            sesion_iniciada = Utildades.crear_usuario(ref usuario_activo);
-                            break;
-                        }
-                        case "Salir":
-                        {
-                            continuar = false;
-                            break;
+                            case "Iniciar Secion":
+                            {
+                                sesion_iniciada = Utildades.Iniciar_Sesion(ref usuario_activo);
+                                break;
+                            }
+                            case "Crear Usuario":
+                            {
+                                sesion_iniciada = Utildades.crear_usuario(ref usuario_activo);
+                                break;
+                            }
+                            case "Salir":
+                            {
+                                continuar = false;
+                                break;
+                            }
                         }
                     }
-                    if (sesion_iniciada)
+                    else 
                     {
-                        Console.WriteLine($"Se inicio secion con exito con el usuario {usuario_activo.nombre_usuario}");
-                        Console.ReadKey();
+                        cadena = Utildades.menu( new String[] { "Buscar pelicula","Buscar serie","Buscar usuario","Ver datos de usuario","Ver publicaciones","Adivinar pelicula","Cerrar secion" });
+                        switch (cadena)
+                        {
+                            case "Buscar pelicula":
+                            {
+                                //se espera codigo
+                                break;
+                            }
+                            case "Buscar serie":
+                            {
+                                //se espera codigo
+                                break;
+                            }
+                            case "Buscar usuario":
+                            {
+                                //se espera codigo
+                                break;
+                            }
+                            case "Ver publicaciones":
+                            {
+                                //se espera codigo
+                                break;
+                            }
+                            case "Adivinar pelicula":
+                            {
+                                //se espera codigo
+                                break;
+                            }
+                            case "Cerrar secion":
+                            {
+                                //se espera codigo
+                                break;
+                            }
+                        }
                     }
-            }
-                int op,elec;
-                Console.WriteLine("Seleccione el tipo de búsqueda:");
-                Console.WriteLine("1. Buscar serie");
-                Console.WriteLine("2. Buscar Pelicula");
-
-                while (!int.TryParse(Console.ReadLine(), out elec))
-                {
-                    Console.WriteLine("Ingrese un número válido:");
-                }
-
-                while (elec < 1 || elec > 2)
-                {
-                    Console.WriteLine("Opción inválida. Ingrese 1 o 2:");
-                    int.TryParse(Console.ReadLine(), out elec);
-                }
-                
-                switch (elec)
-                {
-                    case 1:
+                    int op,elec;
+                    Console.WriteLine("Seleccione el tipo de búsqueda:");
+                    Console.WriteLine("1. Buscar serie");
+                    Console.WriteLine("2. Buscar Pelicula");
+                    while (!int.TryParse(Console.ReadLine(), out elec))
+                    {
+                        Console.WriteLine("Ingrese un número válido:");
+                    }
+                    while (elec < 1 || elec > 2)
+                    {
+                        Console.WriteLine("Opción inválida. Ingrese 1 o 2:");
+                        int.TryParse(Console.ReadLine(), out elec);
+                    } 
+                    switch (elec)
+                    {
+                        case 1:
                         {
                             BuscarSerie Serie = new BuscarSerie();
-                            
                             break;
                         }
-                    case 2:
+                        case 2:
                         {
                             Console.WriteLine("holaPEPE");
                             break;
                         }
+                    }
+                    Console.ReadKey();
                 }
-                Console.ReadKey();
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+        }
     }
-}
 }
--- Usuario.cs
+++ Usuario.cs
@@ -10,29 +10,35 @@ namespace administrador_contenido
         publico,
         inexistente
     }
+
     internal class Usuario
     {
         private string? _nombre_Usuario;
         private string? _clave_usuario;
-        private int _edad;
+        private DateTime _fechaNacimiento;
+        private estado_usuario _estado;
 
         public Usuario()
         {
-            this.Edad = 0;
-            this.Clave_usuario =string.Empty;
+            this.Clave_usuario = string.Empty;
             this.nombre_usuario = string.Empty;
+            this.estado = estado_usuario.inexistente;
+            this.FechaNacimiento = DateTime.Now;
         }
 
-        public Usuario(string nom, string clave, int ed)
+        public Usuario(string nom, string clave, DateTime fechaNac, estado_usuario est)
         {
-            this.Edad = ed;
             this.Clave_usuario = clave;
             this.nombre_usuario = nom;
+            this.FechaNacimiento = fechaNac;
+            this.estado = est;
         }
 
-        public string nombre_usuario 
-        { get {return this._nombre_Usuario; }
-          set {this._nombre_Usuario = value; } }
+        public string nombre_usuario
+        {
+            get { return this._nombre_Usuario; }
+            set { this._nombre_Usuario = value; }
+        }
 
         public string Clave_usuario
         {
@@ -40,18 +46,77 @@ namespace administrador_contenido
             set { this._clave_usuario = value; }
         }
 
+        public DateTime FechaNacimiento
+        {
+            get { return this._fechaNacimiento; }
+            set { this._fechaNacimiento = value; }
+        }
+
+        // Propiedad de solo lectura
         public int Edad
         {
-            get { return this._edad; }
-            set { this._edad = value; }
+            get
+            {
+                int edad = DateTime.Now.Year - FechaNacimiento.Year;
+
+                if (DateTime.Now < FechaNacimiento.AddYears(edad))
+                {
+                    edad--;
+                }
+
+                return edad;
+            }
         }
 
-        public void CrearCuenta()
+        public estado_usuario estado
         {
+            get { return this._estado; }
+            set { this._estado = value; }
         }
 
-        public void IniciarSesion()
+        public void MostrarDatos()
         {
+            Console.WriteLine($"Nombre de usuario: {nombre_usuario}\nClave: {Clave_usuario}\nFecha de nacimiento: {FechaNacimiento:dd/MM/yyyy}\nEdad: {Edad}\nEstado: {estado}");
+        }
+
+        public void CambiarDatos()
+        {
+            //se cambiara proximamente
+            String cadena;
+            cadena = Utildades.menu( new String[] { "Buscar pelicula","Buscar serie","Buscar usuario","Ver datos de usuario","Ver publicaciones","Adivinar pelicula","Cerrar secion" });
+            switch (cadena)
+            {
+                case "Buscar pelicula":
+                {
+                    //se espera codigo
+                    break;
+                }
+                case "Buscar serie":
+                {
+                    //se espera codigo
+                    break;
+                }
+                case "Buscar usuario":
+                {
+                    //se espera codigo
+                    break;
+                }
+                case "Ver publicaciones":
+                {
+                    //se espera codigo
+                    break;
+                }
+                case "Adivinar pelicula":
+                {
+                    //se espera codigo
+                    break;
+                }
+                case "Cerrar secion":
+                {
+                    //se espera codigo
+                    break;
+                }
+            }
         }
     }
 }
