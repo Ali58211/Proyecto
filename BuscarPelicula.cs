50ed38e3f891a1f16715b3f79a17e2b11739ed80
julianbaldelom4r <142670015+julianbaldelom4r@users.noreply.github.com>
2026-06-15 10:14:28 -0300

    Update BuscarPelicula.cs
    agregando opciones de busqueda peliculas

--- BuscarPelicula.cs
+++ BuscarPelicula.cs
@@ -6,6 +6,43 @@ using System.Text.Json;
 namespace administrador_contenido
 {
     internal async Task class BuscarPelicula
-    {
+    
+{
+  
+        public static void BuscarPorTitulo(List<Pelicula> peliculas, string titulo)
+        {
+            foreach (Pelicula pelicula in peliculas)
+            {
+                if (pelicula.title.ToLower().Contains(titulo.ToLower()))
+                {
+                    MostrarPelicula(pelicula);
+                }
+            }
+        }
+
+      
+        
+
+        public static void BuscarPorFecha(List<Pelicula> peliculas, DateTime fecha)
+        {
+            foreach (Pelicula pelicula in peliculas)
+            {
+                if (pelicula.release_date.Date == fecha.Date)
+                {
+                    MostrarPelicula(pelicula);
+                }
+            }
+        }
+
+        private static void MostrarPelicula(Pelicula pelicula)
+        {
+            Console.WriteLine("----------------------");
+            Console.WriteLine($"Título: {pelicula.title}");
+            Console.WriteLine($"Fecha estreno: {pelicula.release_date:dd/MM/yyyy}");
+        }
+    }
+}
+       
+        
     }
 }
