32fd5f067226c7525a2d8a6113f0e824bbf7b2e9
julianbaldelom4r <142670015+julianbaldelom4r@users.noreply.github.com>
2026-06-15 09:52:12 -0300

    Update Biblioteca.cs
    agregado lista publicar publicaciones

--- Biblioteca.cs
+++ Biblioteca.cs
@@ -7,6 +7,8 @@ namespace administrador_contenido
     internal static class Biblioteca
     {
         private static List<Usuario> _usuarios=new List<Usuario>();
+        public List<Publicacion> publicaciones { get; set; } = new List<Publicacion>();
+
         public static void agregar_usuario(Usuario us)
         {
             _usuarios.Add(us);
