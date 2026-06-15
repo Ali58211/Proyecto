a4886ca0ad28e1f6325ab42cd4ff3217d6330233
Ali58211 <Aliibrahimcanario@gmail.com>
2026-06-15 20:21:50 -0300

    Refactor search logic for series and movies

--- Program.cs
+++ Program.cs
@@ -31,18 +31,91 @@ namespace administrador_contenido
                         int.TryParse(Console.ReadLine(), out elec);
                     } 
                     switch (elec)
-                    {
-                        case 1:
-                        {
-                            BuscarSerie Serie = new BuscarSerie();
-                            break;
-                        }
-                        case 2:
-                        {
-                            BuscarSerie Serie = new BuscarPelicula();
-                            break;
-                        }
-                    }
+{
+    case 1:
+        {
+            BuscarSerie Serie = new BuscarSerie();
+
+            Console.WriteLine("Ingrese la serie a buscar:");
+            string titulo_s = Console.ReadLine();
+
+            string url = $"https://api.themoviedb.org/3/search/tv?query={Uri.EscapeDataString(titulo_s)}&language=es-ES&api_key={apiKey}";
+
+            string json = await client.GetStringAsync(url);
+
+            var opciones = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            var respuesta = JsonSerializer.Deserialize<RespuestaSerie>(json, opciones);
+
+            if (respuesta != null && respuesta.results.Count > 0)
+            {
+                foreach (var serie in respuesta.results)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Titulo -- {serie.name}");
+
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"Fecha de lanzamiento -- {serie.first_air_date}");
+
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine($"Descripción -- {serie.overview}");
+
+                    Console.WriteLine("---------------------------");
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("No se encontraron series.");
+            }
+
+            break;
+        }
+    case 2:
+        {
+            BuscarPelicula Pelicula = new BuscarPelicula();
+            Console.WriteLine("Ingrese la pelicula a buscar:");
+            string titulo_p = Console.ReadLine();
+
+            string url_peli = $"https://api.themoviedb.org/3/search/movie?query={Uri.EscapeDataString(titulo_p)}&language=es-ES&api_key={apiKey}";
+
+            string json_1 = await client.GetStringAsync(url_peli);
+
+            var opciones = new JsonSerializerOptions // var deja que el programa infiera el tipo de dato, en este caso RespuestaPelicula
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            var respuesta = JsonSerializer.Deserialize<RespuestaPelicula>(json_1, opciones);
+
+            if (respuesta != null && respuesta.results.Count > 0)
+            {
+                foreach (var peli in respuesta.results)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Titulo -- {peli.title}");
+
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine($"Fecha de lanzamiento -- {peli.release_date}");
+
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine($"Descripción -- {peli.overview}");
+
+                    Console.WriteLine("---------------------------");
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("No se encontraron resultados");
+            }
+
+            break;
+        }
+}
                     Console.ReadKey();
                 }
                 catch (Exception ex)
